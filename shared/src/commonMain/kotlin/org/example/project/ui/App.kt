6af6c3cb8b945aa724c0f75f730d2de859017a63
Maltsev Timur <methyapple@gmail.com>
2026-07-09 22:31:28 +0300

    feat: text "Made by capyplusplus" can be seen for a couple of seconds

--- shared/src/commonMain/kotlin/org/example/project/ui/App.kt
+++ shared/src/commonMain/kotlin/org/example/project/ui/App.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.visible
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,19 +16,28 @@ import androidx.compose.ui.tooling.preview.Preview
 import org.example.project.model.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.Delay
+import kotlinx.coroutines.delay
+import kotlin.time.Duration.Companion.milliseconds
 
 val Folders = listOf<Folder>(
     Folder("Math", 1, Position(30F, 30F)),
     Folder("Eng", 2, Position(200F, 100F))
 )
 
-
+var showLabel by mutableStateOf(true)
 
 @Composable
 @Preview
 fun App() {
     var camera by remember {
         mutableStateOf(Camera(0f, 0f))
+    }
+
+    LaunchedEffect(Unit) {
+        delay(5000.milliseconds)
+        showLabel = false
     }
 
     Surface(modifier = Modifier.fillMaxSize(),
@@ -53,6 +63,11 @@ fun App() {
             Text("Baobab\nv 0.1",
                 modifier = Modifier.align(Alignment.BottomStart),
                 color = Color(200, 200, 200))
+            Text("Made by capyplusplus",
+                fontSize = 30.sp,
+                color = Color(200, 200, 200),
+                modifier = Modifier.align(Alignment.BottomCenter)
+                    .visible(showLabel))
         }
     }
 }
